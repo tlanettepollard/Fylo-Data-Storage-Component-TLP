493d71503603af877cd73dbcf3bd80e82e641756
T. Lanette <TLPollard4516@gmail.com>
2020-08-01 18:36:37 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*document.getElementById('mySlider').oninput = function() {
+    this.style.background = 'linear-gradient(to right, hsl(6, 100%, 80%) 0%, hsl(335, 100%, 65%)' + (((this.value - this.min)/(this.max - this.min) * 100)) + '%, #0c122c ' + (((this.value - this.min)/(this.max - this.min) * 100))+ '%, #0c122c 100%)'
+};
+
+let slider = document.getElementById('mySlider');
+slider.addEventListener('input', sliderChange);
+
+function sliderChange() {
+    let output = document.getElementById('range-value');
+    output.innerHTML = slider.value;
+
+    let gigbLeft = document.getElementById('gigb-value');
+    gigbLeft.innerHTML = parseInt(this.max) - parseInt(slider.value);
+}
+*/
+
+
+
+$('#range').on("input", function() {
+    $(".output").val(this.value);
+}).trigger("change");
--- style-guide.md
+++ style-guide.md
@@ -30,3 +30,6 @@ The designs were created to the following widths:
 
 - Family: [Raleway](https://fonts.google.com/specimen/Raleway)
 - Weights: 400, 700
+
+
+815
